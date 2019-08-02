2ee6f9731499e7296c877b6bd8d1aac250415e91
* <samkb420>
2019-08-02 18:44:11 +0300

    modified rid.py

--- rid.py
+++ rid.py
@@ -1,2 +1,28 @@
-inro="[×]welcome to world of wits[×]"
-print (inro)
+
+#intro
+import time
+print("")
+print("     [+]Welcome To World Of Wits[+]")
+print("              ¯\_(ツ)_/¯           ")
+print("             E.n.j.o.y ):-         ")
+#my info 
+print("")
+print("[-]Author:-samkb420  --")
+print("[+]Email:-kagosamuel07@yahoo.com --")
+print("[-]Github:https://samkb420.github.io/Mysite/")
+print("")
+
+user=input("[√]Enter your name:")
+print("[ Welcome ",user,"]")
+time.sleep(1)
+word="keyboard"
+guess=""
+print("[Π]Game starting..........")
+time.sleep(1)
+print("")
+print("[√]I have keys but no locks, space but no room. You can enter but can’t go outside.[√]")
+print("")
+
+while not guess.lower()==word:
+	guess=input("[+] Answer-:")
+print("You won )-:")
